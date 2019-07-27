866af3a2731abf0d35a84debf1f61a3aff930b93
anower00 <anower.hasan@gmail.com>
2019-07-27 17:34:26 +0600

    validation for resume and profile picture formate

--- resources/views/user/edit_profile.blade.php
+++ resources/views/user/edit_profile.blade.php
@@ -1,10 +1,12 @@
 @extends('layouts.app')
 @section('content')
+
     @include('layouts.sidebar')
     <div class="content">
         @if(Session::has('message'))
             <p class="alert {{ Session::get('alert-class', 'alert-info') }}" style="margin-top: 80px">{{ Session::get('message') }}</p>
         @endif
+
         <h2 style="margin-top: 56px">{{ $user->first_name.' '. $user->last_name}}</h2>
 
     <form method="post" action="{{ route('update_profile') }}" enctype="multipart/form-data">
@@ -12,6 +14,9 @@
         @csrf
         <img src="{{ asset($user->profile_picture) }}" class="img-responsive section-icon hidden-sm hidden-xs menu_img">
         <br>
+        @if ($errors->has('image'))
+            <div class="error text-danger">{{ $errors->first('image') }}</div>
+        @endif
         <input type="file" name="image">
         <br><br>
         Skills (comma separated):<br>
@@ -20,6 +25,9 @@
         Email: <br>
         <label>{{ $user->email }}</label>
         <br><br>
+        @if ($errors->has('resume'))
+            <div class="error text-danger">{{ $errors->first('resume') }}</div>
+        @endif
         Upload Resume:<br>
         <input type="file" name="resume">
         <br><br>
