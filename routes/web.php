f52e1ad540ff0594417d06c0dada5fb12a678f32
anower00 <anower.hasan@gmail.com>
2019-07-27 17:16:25 +0600

    route grouping for authenticated user

--- routes/web.php
+++ routes/web.php
@@ -16,14 +16,14 @@ Route::get('/', 'HomeController@welcome')->name('/');
 
 Auth::routes();
 
-Route::group(['middleware'=>'Access:1'], function(){
+Route::group(['middleware'=>'Access:1' ,'middleware' => 'auth'], function(){
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/edit_profile', 'UserController@editProfile')->name('edit_profile');
 Route::PUT('/update_profile', 'UserController@updateProfile')->name('update_profile');
 Route::get('/job/apply/{id}', 'JobController@apply_job')->name('apply_job');
 });
 
-Route::group(['middleware'=>'Access:2'], function(){
+Route::group(['middleware'=>'Access:2','middleware' => 'auth'], function(){
 Route::get('/company', 'JobController@company')->name('company');
 Route::get('/job/add', 'JobController@add_job')->name('add_job');
 Route::post('/job/store', 'JobController@job_store')->name('job_store');
